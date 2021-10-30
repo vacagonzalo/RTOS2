6c3b4abb06d1e83e816b18c4d98caa28add86357
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-10-30 09:16:01 -0300

    out and vscode

--- src/template.c
+++ src/template.c
@@ -26,26 +26,27 @@
 
 /*=====[Main function, program entry point after power on or reset]==========*/
 
-int main( void )
+int main(void)
 {
    boardInit();
 
    // Create a task in freeRTOS with dynamic memory
    xTaskCreate(
-      myTask,                     // Function that implements the task.
-      (const char *)"myTask",     // Text name for the task.
-      configMINIMAL_STACK_SIZE*2, // Stack size in words, not bytes.
-      0,                          // Parameter passed into the task.
-      tskIDLE_PRIORITY+1,         // Priority at which the task is created.
-      0                           // Pointer to the task created in the system
+       myTask,                       // Function that implements the task.
+       (const char *)"myTask",       // Text name for the task.
+       configMINIMAL_STACK_SIZE * 2, // Stack size in words, not bytes.
+       0,                            // Parameter passed into the task.
+       tskIDLE_PRIORITY + 1,         // Priority at which the task is created.
+       0                             // Pointer to the task created in the system
    );
 
    vTaskStartScheduler(); // Initialize scheduler
 
-   while( true ); // If reach heare it means that the scheduler could not start
+   while (true)
+      ; // If reach heare it means that the scheduler could not start
 
    // YOU NEVER REACH HERE, because this program runs directly or on a
-   // microcontroller and is not called by any Operating System, as in the 
+   // microcontroller and is not called by any Operating System, as in the
    // case of a PC program.
    return 0;
 }
