af7e8004ba4d75bd420f8f378424a05f318e0c34
Carlos Maffrand <carlosmaffrand5@gmail.com>
2021-10-30 11:05:32 -0300

    Se agrega tempalte a C1

---
+++ inc/C1.h
@@ -0,0 +1,44 @@
+/*=============================================================================
+ * Copyright (c) 2021, Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
+ *                     Lucas Zalazar <lucas.zalazar6@gmail.com>
+ *                     Carlos Maffrand <carlosmaffrand5@gmail.com>
+ * All rights reserved.
+ * License: mit (see LICENSE.txt)
+ * Date: 2021/10/30
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __C1_H__
+#define __C1_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __C1_H__ */
