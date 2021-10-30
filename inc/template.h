548ce1b85da163bff4086182d8edb9b25229c022
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-10-30 09:05:22 -0300

    template

---
+++ inc/template.h
@@ -0,0 +1,42 @@
+/*=============================================================================
+ * Copyright (c) 2021, Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
+ * All rights reserved.
+ * License: mit (see LICENSE.txt)
+ * Date: 2021/10/30
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __TEMPLATE_H__ */
